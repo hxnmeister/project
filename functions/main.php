52b4b3fc63696fe6fb26913d3ea46c0ad25a53aa
Valerii <valeriy.stronskiy@gmail.com>
2023-09-21 20:45:59 +0300

    Classwork

--- functions/main.php
+++ functions/main.php
@@ -98,40 +98,6 @@
         }
     }
 
-    function uploadImage()
-    {
-        $allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/jpg', 'image/avif'];
-
-        dump($_FILES['file']);
-
-        extract($_FILES['file']); //$name, $full_path, $type, $tmp_name, $error, $size
-
-        if($error === 4)
-        {
-            Message::set('File is required!', 'danger');
-            redirect("uploads");
-        }
-        elseif($error !== 0)
-        {
-            Message::set('File is not uploaded!', 'danger');
-            redirect('uploads');
-        }
-
-        if(in_array($type, $allowedTypes))
-        {
-            $fName = uniqid().'__'.session_id().'.'.end(explode('.', $name));
-            move_uploaded_file($tmp_name, './uploadedImages/'.$fName);
-        
-            Message::set('File is successfully uploaded!');
-        }
-        else
-        {
-            Message::set('Such type not allowed!', 'danger');
-        }
-
-        redirect('uploads');
-    }
-
     function uploadFewImages()
     {
         $allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/jpg', 'image/avif'];
@@ -188,3 +154,89 @@
 
         redirect('custom-dir-upload');
     }
+
+    function uploadImage()
+    {
+        $allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/jpg', 'image/avif'];
+
+        dump($_FILES['file']);
+
+        extract($_FILES['file']); //$name, $full_path, $type, $tmp_name, $error, $size
+
+        if($error === 4)
+        {
+            Message::set('File is required!', 'danger');
+            redirect("uploads");
+        }
+        elseif($error !== 0)
+        {
+            Message::set('File is not uploaded!', 'danger');
+            redirect('uploads');
+        }
+
+        if(in_array($type, $allowedTypes))
+        {
+            $fName = uniqid().'__'.session_id().'.'.end(explode('.', $name));
+            move_uploaded_file($tmp_name, './uploadedImages/'.$fName);
+        
+            Message::set('File is successfully uploaded!');
+        }
+        else
+        {
+            Message::set('Such type not allowed!', 'danger');
+        }
+
+        resizeImage('./uploadedImages/'.$fName, 150, true);
+        resizeImage('./uploadedImages/'.$fName, 300);
+
+        redirect('uploads');
+    }
+
+    function resizeImage(string $path, int $size = 200, bool $crop = false) : void
+    {
+        extract(pathinfo($path));
+
+        $extension = strtolower($extension) === 'jpg' ? 'jpeg' : strtolower($extension);
+        $functionCreate = 'imagecreatefrom'.$extension;
+        $src = $functionCreate($path);
+        list($src_width, $src_height) = getimagesize($path);
+
+        if($crop)
+        {
+            //Жорстка обрізка
+            
+            $dest = imagecreatetruecolor($size, $size);
+
+            if($src_width > $src_height)
+            {
+                imagecopyresized($dest, $src, 0, 0, $src_width / 2 - $src_height / 2, 0, $size, $size, $src_height, $src_height);
+            }
+            elseif($src_width < $src_height)
+            {
+                imagecopyresized($dest, $src, 0, 0, 0, $src_height / 2 - $src_width / 2,  $size, $size, $src_width, $src_width);
+            }
+        }
+        else
+        {
+            //Пропорційне зменшення
+
+            $dest_width = $size;
+            $dest_height = $size * $src_height / $src_width;
+
+            $dest = imagecreatetruecolor($dest_width, $dest_height);
+            imagecopyresized($dest, $src, 0, 0, 0, 0, $dest_width, $dest_height, $src_width, $src_height);
+        }
+
+        $saveDir = $crop ? 'small' : 'medium';
+
+        if($extension === 'jpg')
+        {
+            ('image'.$extension)($dest, "$dirname/$saveDir/$basename", 100);
+        }
+        else
+        {
+            ('image'.$extension)($dest, "$dirname/$saveDir/$basename");
+        }
+    }
+
+    
--- pages/uploads.php
+++ pages/uploads.php
@@ -4,6 +4,41 @@
 
 <form action="/uploads" method="post" enctype="multipart/form-data">
     <input class="mt-3" type="file" name="file">
-    <br>
-    <button class="btn btn-outline-primary mt-5" name="action" value="uploadImage">Load File</button>
+    <button class="btn btn-outline-primary" name="action" value="uploadImage">Load File</button>
 </form>
+
+<?php
+    // $files = array_diff(scandir('./uploadedImages'), ['.', '..']);
+
+    // foreach($files as $file)
+    // {
+    //     if(!is_dir("./uploadedImages/$file"))
+    //     {
+    //         echo "<img src='./uploadedImages/$file' alt=''>";
+    //     }
+    // }
+    //----------------------------------------------------------------
+
+    // $dir = opendir('./uploadedImages');
+    
+    // while($file = readdir($dir))
+    // {
+    //     echo $file.'<br>';
+    // }
+
+    // closedir($dir);
+    //----------------------------------------------------------------
+        
+    // $files = glob('./uploadedImages/*', GLOB_ONLYDIR);
+    // $files = glob('./uploadedImages/*.{jpg,jpeg,png,gif,webp,avif}', GLOB_BRACE);
+
+
+    $files = array_diff(scandir('./uploadedImages'), ['.', '..']);
+
+    foreach($files as $file)
+    {
+        if(!is_dir("./uploadedImages/$file"))
+        {
+            echo "<a href='./uploadedImages/$file'><img src='./uploadedImages/small/$file' alt=''></a>";
+        }
+    }
